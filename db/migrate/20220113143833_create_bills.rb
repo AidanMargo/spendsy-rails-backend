daf37d9927a2c2667c32aa05cb0cce5f1599ae04
Aidan Margo <aidanmargo@gmail.com>
2022-03-09 21:30:02 -0600

    updated to postgresql, copied to allow for deploying to heroku

--- db/migrate/20220113143833_create_bills.rb
+++ db/migrate/20220113143833_create_bills.rb
@@ -2,9 +2,9 @@ class CreateBills < ActiveRecord::Migration[6.1]
   def change
     create_table :bills do |t|
       t.string :bill_name
-      t.integer :bill_amount
+      t.integer :bill_amount, :limit => 4
       t.string :category_name
-      t.integer :wallet_id
+      t.integer :wallet_id, :limit => 4
 
       t.timestamps
     end
--- db/migrate/20220113144638_create_wallets.rb
+++ db/migrate/20220113144638_create_wallets.rb
@@ -2,8 +2,8 @@ class CreateWallets < ActiveRecord::Migration[6.1]
   def change
     create_table :wallets do |t|
       t.string :name
-      t.integer :user_id
-      t.integer :amount
+      t.integer :user_id, :limit => 4
+      t.integer :amount, :limit => 4
 
       t.timestamps
     end
